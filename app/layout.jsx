d14487325a019dd3c42b7afa6c08a4e7380b8412
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-02 13:16:03 +0300

    font added

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,8 +1,21 @@
+import { Inter } from "@next/font/google";
+import styles from "./styles.module.css";
+import "../styles/global.css";
+
+const interFont = Inter();
+
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <head />
-      <body>{children}</body>
+    <html lang="tr" className={interFont.className}>
+      <body className={styles.container}>
+        <header>
+          <p>Header</p>
+        </header>
+        <main>{children}</main>
+        <footer>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
   );
 }
