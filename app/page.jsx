92d3d74f1e7981207c213d843e7cc03a882d47ea
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-04 14:23:18 +0300

    fetch dummy API

--- app/page.jsx
+++ app/page.jsx
@@ -1,9 +1,20 @@
 import BlogCard from "../components/BlogCard";
+import styles from "./styles.module.css";
+async function getPosts() {
+  const response = await fetch("https://dummyjson.com/posts?limit=10");
+  return response.json();
+}
 
-export default function Home() {
+export default async function Home() {
+  const {posts} = await getPosts();
+
+  console.log(posts);
+
   return (
-    <div>
-      <BlogCard />
+    <div className={styles.blogsContainer}>
+      {posts.map((post) => (
+        <BlogCard key={post.id} {...post} />
+      ))}
     </div>
   );
 }
--- components/BlogCard/index.js
+++ components/BlogCard/index.js
@@ -1,22 +1,25 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
-function BlogCard () {
-    return (
-    <Link href='/' className={styles.card}>
-        <div className={styles.cardImage}>
-           <Image src='https://picsum.photos/200/300' alt="blog card image"  fill/> {/* alt tag is required for next.js */}
-        </div>
-        <div>
-           <h3>
-            title
-           </h3>
-           <p>Lorem, ipsum.</p>
-        </div>
+function BlogCard({ title, body, id }) {
+  return (
+    <Link href="/" className={styles.card}>
+      <div className={styles.cardImage}>
+        <Image
+          src={`https://picsum.photos/200/300?random=${id}`}
+          alt="blog card image"
+          fill
+        />{" "}
+        {/* alt tag is required for next.js */}
+      </div>
+      <div>
+        <h3>{title}</h3>
+        <p>{body}.</p>
+      </div>
     </Link>
-    )
+  );
 }
 
 export default BlogCard;
