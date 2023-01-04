58520956caea985ccd26455104f8c5fc3a16be1c
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-04 17:45:22 +0300

    error & not-found added

---
+++ app/[id]/page.js
@@ -0,0 +1,36 @@
+import { notFound } from "next/navigation";
+import React from "react";
+import Image from "next/image";
+
+import styles from "./styles.module.css";
+
+async function getPost(id) {
+  try {
+    const response = await fetch(`https://dummyjson.com/posts/${id}`);
+    return response.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+export default async function Page({ params }) {
+  const { id, title, body } = await getPost(params.id);
+
+  if (!title) {
+    return notFound();
+  }
+
+  return (
+    <div className={styles.blogContainer}>
+      <div className={styles.cardImage}>
+        <Image
+          src={`https://picsum.photos/960/400?random=${id}`}
+          alt={title}
+          fill
+        />
+      </div>
+      <h1 className={styles.title}>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
+}
--- app/page.js
+++ app/page.js
@@ -1,14 +1,13 @@
-import BlogCard from "../components/BlogCard";
 import styles from "./styles.module.css";
+import BlogCard from "../components/BlogCard";
+
 async function getPosts() {
   const response = await fetch("https://dummyjson.com/posts?limit=10");
   return response.json();
 }
 
 export default async function Home() {
-  const {posts} = await getPosts();
-
-  console.log(posts);
+  const { posts } = await getPosts();
 
   return (
     <div className={styles.blogsContainer}>
